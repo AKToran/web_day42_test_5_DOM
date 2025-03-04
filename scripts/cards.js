42f68806c361c3245cb82038feb96b1d22768ea0
AKToran <aktoran60@gmail.com>
2025-03-04 17:38:31 +0600

    complete task and add activity log

---
+++ scripts/cards.js
@@ -0,0 +1,47 @@
+document.getElementById('btn-1').addEventListener('click', function(event){
+  disableButton('btn-1');
+  setActivityLog('card-title-1');
+  decrementTaskAssigned();
+  incrementTaskCompleted();
+  alert("You have successfully completed a task");
+})
+
+document.getElementById('btn-2').addEventListener('click', function(event){
+  disableButton('btn-2');
+  setActivityLog('card-title-2');
+  decrementTaskAssigned();
+  incrementTaskCompleted();
+  alert("You have successfully completed a task");
+})
+
+document.getElementById('btn-3').addEventListener('click', function(event){
+  disableButton('btn-3');
+  setActivityLog('card-title-3');
+  decrementTaskAssigned();
+  incrementTaskCompleted();
+  alert("You have successfully completed a task");
+})
+
+document.getElementById('btn-4').addEventListener('click', function(event){
+  disableButton('btn-4');
+  setActivityLog('card-title-4');
+  decrementTaskAssigned();
+  incrementTaskCompleted();
+  alert("You have successfully completed a task");
+})
+
+document.getElementById('btn-5').addEventListener('click', function(event){
+  disableButton('btn-5');
+  setActivityLog('card-title-5');
+  decrementTaskAssigned();
+  incrementTaskCompleted();
+  alert("You have successfully completed a task");
+})
+
+document.getElementById('btn-6').addEventListener('click', function(event){
+  disableButton('btn-6');
+  setActivityLog('card-title-6');
+  decrementTaskAssigned();
+  incrementTaskCompleted();
+  alert("You have successfully completed a task");
+})
---
+++ utilities/utilities.js
@@ -0,0 +1,31 @@
+function disableButton(id){
+  document.getElementById(id).setAttribute('disabled', true);
+}
+
+function decrementTaskAssigned(){
+  const strTasks = document.getElementById('task-assigned').innerText;
+  let tasks = parseInt(strTasks);
+  tasks -= 1;
+  document.getElementById('task-assigned').innerText = tasks;
+}
+
+function incrementTaskCompleted(){
+  const strTasks = document.getElementById('task-completed').innerText;
+  let tasks = parseInt(strTasks);
+  tasks += 1;
+  document.getElementById('task-completed').innerText = tasks;
+}
+
+function setActivityLog(id){
+  const title = document.getElementById(id).innerText;
+  const time = new Date().toLocaleTimeString();
+  let activityDiv = document.getElementById('activity-log');
+  let div = document.createElement('div');
+  div.classList.add('m-2', 'p-2', 'rounded-xl', 'bg-slate-100');
+  div.innerHTML = `
+  <p>You have completed the task <span class="font-bold"> ${title} </span> at ${time}.</p>
+  `
+  activityDiv.appendChild(div);
+
+  console.log(div);
+}
